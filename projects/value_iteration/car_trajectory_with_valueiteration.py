98293c7d9cf5531a8f6f56d1964f071772a6b0be
amy-f <frappier.amelie@gmail.com>
2019-11-08 15:19:26 -0500

    End of car testing

--- projects/value_iteration/car_trajectory_with_valueiteration.py
+++ projects/value_iteration/car_trajectory_with_valueiteration.py
@@ -26,8 +26,8 @@ sys  = vehicle.KinematicCarModelwithObstacles()
 sys.x_ub = np.array([+35,+3.5,+3])
 sys.x_lb = np.array([-5,-2,-3])
 
-sys.u_ub = np.array( [+2,+0.3] )
-sys.u_lb = np.array( [-2,-0.3] )
+sys.u_ub = np.array( [+3,+1] )
+sys.u_lb = np.array( [-3,-1] )
 
 # Discrete world
 grid_sys = discretizer.GridDynamicSystem( sys , (51, 51, 21) , (3,3) , 0.1 )
@@ -40,7 +40,7 @@ cf = costfunction.QuadraticCostFunction(
 
 cf.xbar = np.array( [30, 0, 0] ) # target
 cf.INF  = 1E8
-cf.EPS  = 0.2
+cf.EPS  = 0
 cf.R    = np.array([[0.1,0],[0,0]])
 
 # VI algo
@@ -50,23 +50,23 @@ vi = valueiteration.ValueIteration_ND( grid_sys , cf )
 vi.uselookuptable = True
 vi.initialize()
 #if load_data:
-vi.load_data('car_vi_3')
-vi.compute_steps(200, plot=True, maxJ=6000)
+vi.load_data('car_vi_7')
+# vi.compute_steps(30, plot=True, maxJ=6000)
 #if save_data:
-vi.save_data('car_vi_3')
+vi.save_data('car_vi')
 
 vi.assign_interpol_controller()
 
-vi.plot_cost2go(maxJ=6000)
+vi.plot_cost2go(maxJ=50000)
 vi.plot_policy(0)
 vi.plot_policy(1)
 
 cl_sys = controller.ClosedLoopSystem( sys , vi.ctl )
 #
 ## Simulation and animation
-x0   = [0, 0 ,0]
-tf   = 20
+x0   = [8, -2, 0]
+tf   = 10
 
-sim = cl_sys.compute_trajectory(x0, tf, 10001)
+sim = cl_sys.compute_trajectory(x0, tf, 10001, 'euler')
 cl_sys.get_plotter().plot(sim, 'xu')
-cl_sys.get_animator().animate_simulation(sim, save=True, file_name='car_3')
+cl_sys.get_animator().animate_simulation(sim, save=False, file_name='car_7-5')
